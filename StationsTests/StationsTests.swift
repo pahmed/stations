ba58bd18ea15b227f63a11b32ce64e7984e230e0
Ahmed Ibrahim <eng.ahmedibrahim16@gmail.com>
2019-01-10 13:09:49 +0200

    Remove a comment

--- StationsTests/StationsTests.swift
+++ StationsTests/StationsTests.swift
@@ -12,15 +12,14 @@ import XCTest
 class StationsTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() {
-        // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
